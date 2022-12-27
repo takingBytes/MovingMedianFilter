6fefa08eb271d1423c3c1cfdc25c88eeeb8fd3e3
takingBytes <93669489+takingBytes@users.noreply.github.com>
2022-12-27 18:50:06 +0100

    further cleanup

--- main.c
+++ main.c
@@ -5,60 +5,33 @@
 #include "median.h"
 #include "sorting.h"
 
-#define MAX_SIZE 9
+#define MAX_SIZE 10
 #define MAX_RUN 100
 
-//float arr[MAX_SIZE] = {1,2,3,4,5,6,7,8,9};
-float arr[MAX_SIZE] = {9,8,7,6,5,4,3,2,1};
+float arr[MAX_SIZE] = {10,9,8,7,6,5,4,3,2,1};
 float* ptArr[MAX_SIZE];
 float raw=0;
 float med;
+
 void main(void)
 {
-   srand(time(NULL));
+    srand(time(NULL));
     int index = 0;
- /*
-  for (int i = 0; i < MAX_RUN; i++) 
-  {
-    float raw =(float) (5* (int)rand() / (int)(RAND_MAX));
-    arr[index] = raw;
-    float med = median(arr, MAX_SIZE);
-    printf("raw;%.0f med;%.0f\n", raw,med);
-    index = (index + 1) % MAX_SIZE;
-  }
-*/
 
     pointerArrayInit(arr,ptArr,MAX_SIZE);
-  for (int i = 0; i < MAX_RUN; i++) 
-  {
-    //pointerArrayInit(arr,ptArr,MAX_SIZE);
-    med = medianPt(ptArr, MAX_SIZE);
-    //printf("\n");
-
-    printf("pt");  
-    for (int i = 0; i < MAX_SIZE; i++) 
+    for (int i = 0; i < MAX_RUN; i++) 
     {
-        printf(";%.f ", *ptArr[i]);
+        med = medianPt(ptArr, MAX_SIZE);
+
+        printf("pt");  
+        for (int i = 0; i < MAX_SIZE; i++) 
+        {
+            printf(";%.f", *ptArr[i]);
+        }
+        printf(";->%.1f\n", med);
+            
+        raw =(float) (10* (int)rand() / (int)(RAND_MAX));
+        arr[index] = raw;
+        index = (index + 1) % MAX_SIZE;
     }
-    printf("\n");
-    
-
-   /* for (int i = 0; i < MAX_SIZE; i++) 
-    {
-        printf("dt:%.1f ", arr[i]);
-    }*/
-
-
-   
-   //printf("raw;%.0f med;%.0f\n", raw,med);
-   
-
-    raw =(float) (10* (int)rand() / (int)(RAND_MAX));
-    arr[index] = raw;
-    index = (index + 1) % MAX_SIZE;
-  }
-  
-
-  //TestSort();
-
 }
