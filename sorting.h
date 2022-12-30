457cec57800c1a2fae3fc02073e77da53a19a81b
takingBytes <93669489+takingBytes@users.noreply.github.com>
2022-12-31 00:12:40 +0100

    remove sorting module

--- sorting.h
+++
@@ -1,7 +0,0 @@
-#ifndef SORT_H
-#define SORT_H
-
-typedef enum {BUBBLE, INSERTION, SHELL} Sorter;
-extern void SortPtBufferInit(float *bufferUnsorted,float **ptBufferSorted,unsigned int size);
-extern unsigned int SortPt(float** ptBufferSorted,Sorter type, unsigned int n);
-#endif
